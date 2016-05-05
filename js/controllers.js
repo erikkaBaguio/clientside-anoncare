8403e4d22d8590cc2d758025a97426caab33a6b3
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-06 03:05:24 +0800

    Added login controller

--- js/controllers.js
+++ js/controllers.js
@@ -1,3 +1,34 @@
 /**
  * Created by irika-pc on 5/6/2016.
+ * Controller - receive data from client
  */
+
+angular.module('myApp').controller('loginController',
+    ['$scope', '$location', 'AuthService',
+        function ($scope, $location, AuthService) {
+
+            $scope.login = function () {
+
+                // initial values
+                $scope.error = false;
+                $scope.disabled = true;
+
+                // call login from service
+                AuthService.login($scope.loginForm.username, $scope.loginForm.password)
+                    // handle success
+                    .then(function () {
+                        $location.path('/dashboard');
+                        $scope.disabled = false;
+                        $scope.loginForm = {};
+                    })
+                    // handle error
+                    .catch(function () {
+                        $scope.error = true;
+                        $scope.errorMessage = "Invalid username and/or password";
+                        $scope.disabled = false;
+                        $scope.loginForm = {};
+                    });
+
+            };
+
+        }]);
