36f997774f8c8bd1476fdbb9bbf84932684fc22d
erikkaBaguio <erikka.baguio@gmail.com>
2016-05-06 03:04:05 +0800

    Modified main.js for routing

--- js/main.js
+++ js/main.js
@@ -1,3 +1,41 @@
 /**
  * Created by irika-pc on 5/6/2016.
  */
+
+/**
+ *Main Module of The Application
+ */
+
+var myApp = angular.module('myApp', ['ui.router']);
+
+myApp.config(function ($stateProvider, $urlRouterProvider) {
+
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+
+    /*Authentication*/
+        .state('login', {
+            url: '/',
+            templateUrl: 'pages/login.html',
+            controller: 'loginController'
+        })
+
+        .state('dashboard', {
+            url: '/dashboard',
+            views: {
+                '': {
+                    templateUrl: 'pages/dashboard.html',
+                    controller: 'NavigationController'
+                },
+                'dashboard@dashboard': {
+                    templateUrl: 'pages/content/dashboard.html',
+                    controller: 'NavigationController'
+                },
+            }
+        })
+        .state('dashboard.assessment-add', {
+            url: '/assessments/add ',
+            templateUrl: 'pages/content/assessment.html'
+        })
+});
