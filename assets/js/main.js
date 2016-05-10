551f4c5b7ef3865c049ff31a012e23b862b7a817
remarcbalisi <remarcbalisi@gmail.com>
2016-05-10 12:34:36 +0800

    add the logout alert

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,6 +21,10 @@ function eraseCookie(name) {
 	$('#admin-page').hide(0);
 	$('#nurse-page-page').hide(0);
 	$('#doctor-page-page').hide(0);
+
+	$('#log-in-alert').html(
+		'<div class="alert alert-success"><strong>Success ' +
+		 '!</strong> Successfully logged out.</div>');
 }
 
 
